12c5a7fb281bcc43c753b9e6193a3c59b54ab44d
Exqra <tcatal99@hotmail.com>
2019-07-11 01:20:43 +0300

    10.07.2019 sabah

--- i2iCellApp/views.py
+++ i2iCellApp/views.py
@@ -6,67 +6,217 @@ import requests
 from django.views.decorators.csrf import csrf_exempt
 import os
 from django.http import QueryDict
+import re
+from datetime import date
+
 # Create your views here.
 
 proxies = {
-  "http": None,
-  "https": None,
+    "http": None,
+    "https": None,
 }
 
-endpoint = 'http://68.183.75.84:8080/i2iCellService/services/Services/login'
+loginEndpoint = 'http://68.183.75.84:8080/i2iCellService/services/Services/login'
+registerEndpoint = 'http://68.183.75.84:8080/i2iCellService/services/Services/createAccount'
+
+
+def loginEncodeUrl(inputPhoneNumber, inputPassword):
+    qd = QueryDict(mutable=True)
+    qd.update(
+        inputPhoneNumber=inputPhoneNumber,
+        inputPassword=inputPassword
+    )
+
+    url = '{}?{}'.format(loginEndpoint, qd.urlencode())
+    return url
+
 
 def login(request):
     post = request.POST.copy()
 
     if post.get('login_button'):
-        qd = QueryDict(mutable=True)
-        qd.update(
-            inputPhoneNumber=request.POST.get('phone_num'),
-            inputPassword=request.POST.get('password')
-        )
 
-        print("---------------You are in")
-        url = '{}?{}'.format(endpoint, qd.urlencode())
-        print("---------------Url: " + url)
-        response = requests.get(url, proxies=proxies)
+        inputPhoneNumber = request.POST.get('phone_num')
+        inputPassword = request.POST.get('password')
 
-        #result = response.text
-        result = returnValidationValue(response.text)
-
-        if(result == '1'):
-            return render(request, 'i2iCellApp/homepage.html')
+        if len(inputPhoneNumber) <= 0 or len(inputPassword) <= 0:
+            messages.error(request, "Bos alan birakilmamalidir!")
+        elif inputPhoneNumber[0] == 0 or len(inputPhoneNumber) != 10:
+            messages.error(request, "Telefon numaranizi basinda 0 olmadan 10 haneli giriniz.")
+        elif not isValidPhoneNumber(inputPhoneNumber):
+            messages.error(request, "Telefon numaraniz sadece rakamlardan olusabilir.")
         else:
-            #Phone number not found
-            #Phone number and password does not match
-            #No connection
-            print(response)
+            try:
+                response = requests.get(loginEncodeUrl(inputPhoneNumber, inputPassword), proxies=proxies)
+            except ConnectionError as e:
+                print(e)
+                messages.error(request, "Internet baglantiniz yok!")
 
-            messages.error(request, "Yanlis Kullanici Numarasi Veya Sifre!")
+            result = returnValidationValue(response.text)
 
-
+            if result == '1':
+                return render(request, 'i2iCellApp/homepage.html')
+            else:
+                messages.error(request, "Yanlis Kullanici Numarasi Veya Sifre!")
 
     elif post.get('forgot_button'):
         return render(request, 'i2iCellApp/forgot_password.html')
     elif post.get('register_button'):
         return render(request, 'i2iCellApp/register.html')
+
     return render(request, 'i2iCellApp/login.html')
 
-def returnValidationValue( response):
-    searchedValue = "<ns:return>"
 
-    lengthOfSeachedValue = len(searchedValue)
+def forgotPassword(request):
+    post = request.POST.copy()
+    if post.get('save_button'):
+        print("forgot in")
+    return render(request, 'i2iCellApp/forgotPassword.html')
 
-    startingIndexOfValue = response.index(searchedValue)
-    lengthOfReturnValue = 1
 
-    startingIndexOfReturnValue = startingIndexOfValue + lengthOfSeachedValue
-    return (response[startingIndexOfReturnValue: startingIndexOfReturnValue + lengthOfReturnValue])
+def registerEncodeUrl(inputFirstName, inputLastName, inputPhoneNumber, inputEmail, inputPassword, inputBirthDate,
+                      inputTcNumber):
+    qd = QueryDict(mutable=True)
+    qd.update(
+        inputFirstName=inputFirstName,
+        inputLastName=inputLastName,
+        inputPhoneNumber=inputPhoneNumber,
+        inputEmail=inputEmail,
+        inputPassword=inputPassword,
+        inputBirthDate=inputBirthDate,
+        inputTcNumber=inputTcNumber
+    )
+
+    url = '{}?{}'.format(registerEndpoint, qd.urlencode())
+    return url
+
+
+def register(request):
+    print("register method in")
+    post = request.POST.copy()
+
+    if post.get('create_user_button'):
+        print("create button pressed")
+
+        day = post.get('day_box', '0')
+        month = post.get('month_box', '0')
+        year = post.get('year_box', '0')
+
+        tcNo = post.get('tc_no')
+        userName = post.get('user_name')
+        userLastName = post.get('user_last_name')
+        birthDate = day + "/" + month + "/" + year
+        phoneNumber = post.get('phone_number')
+        password = post.get('password')
+        email = post.get('email')
+
+        if not isValidTCID(tcNo):
+            messages.error(request, "Verilen Kimlik Numarasi Bilgileri Yanlistir!")
+        elif not isUserOldEnough(12, birthDate):
+            messages.error(request, "Kayit olmak icin yasiniz yetmemektedir.")
+        elif not (len(phoneNumber) > 0 and len(password) > 0 and len(userLastName) > 0 and \
+                  (len(day) > 0 and len(month) > 0 and len(year) > 0) and len(phoneNumber) > 0 and len(password) > 0):
+            messages.error(request, "Bos alan birakilmamalidir!")
+        elif not (phoneNumber[0] != 0 and len(phoneNumber) == 10):
+            messages.error(request, "Telefon numaranizi basinda 0 olmadan 10 haneli giriniz.")
+
+        elif not isValidPhoneNumber(phoneNumber):
+            messages.error(request, "Telefon numaraniz sadece rakamlardan olusabilir.")
+        elif not isValidPassword(password):
+            messages.error(request,
+                           "Sifreniz en az 10 karakterli olmali, kücük harf, büyük harf ve en az bir rakam icermelidir.")
+        elif not isValidEmail(email):
+            messages.error(request, "Bu email adresi gecerli degildir.")
+        else:
+            try:
+                response = requests.get(registerEncodeUrl(userName, userLastName,phoneNumber, email, password, birthDate, tcNo), proxies=proxies)
+            except ConnectionError as e:
+                print(e)
+                messages.error(request, "Internet baglantiniz yok!")
+
+            result = returnValidationValue(response.text)
+
+            if result == '1':
+                return render(request, 'i2iCellApp/homepage.html')
+            else:
+                messages.error(request, "Bu telefon numarasi zaten mevcut.")
+                return render(request, 'i2iCellApp/register.html')
+
+
+    return render(request, 'i2iCellApp/register.html')
+
+
+def returnValidationValue(response):
+    try:
+        searchedValue = "<ns:return>"
+
+        lengthOfSeachedValue = len(searchedValue)
+
+        startingIndexOfValue = response.index(searchedValue)
+        lengthOfReturnValue = 1
+
+        startingIndexOfReturnValue = startingIndexOfValue + lengthOfSeachedValue
+        return (response[startingIndexOfReturnValue: startingIndexOfReturnValue + lengthOfReturnValue])
+    except:
+        return 0
+
 
 def homepage(request):
     return render(request, 'i2iCellApp/homepage.html')
 
-def forgotPassword(request):
-    return render(request, 'i2iCellApp/forgot_password.html')
 
-def register(request):
-    return render(request, 'i2iCellApp/register.html')
+def isValidPhoneNumber(inputPhoneText):
+    return inputPhoneText.isdigit()
+
+
+def isValidTCID(value):
+    value = str(value)
+
+    if not len(value) == 11:
+        return False
+
+    if not value.isdigit():
+        return False
+
+    if int(value[0]) == 0:
+        return False
+
+    digits = [int(d) for d in str(value)]
+
+    if not sum(digits[:10]) % 10 == digits[10]:
+        return False
+
+    if not (((7 * sum(digits[:9][-1::-2])) - sum(digits[:9][-2::-2])) % 10) == digits[9]:
+        return False
+
+    return True
+
+
+def isValidPassword(inputPassword):
+    if (any(x.isupper() for x in inputPassword) and any(x.islower() for x in inputPassword)
+            and any(x.isdigit() for x in inputPassword) and len(inputPassword) >= 10):
+        return True
+    return False
+
+
+def isValidEmail(email):
+    return re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+
+
+def isUserOldEnough(yearConstraint, birthDate):
+    today = date.today().strftime('%Y/%m/%d')
+
+    todayYear = today[0:4]
+    todayMonth = today[5:7]
+    todayDay = today[8:10]
+
+    birthYear = birthDate[6:10]
+    birthMonth = birthDate[3:5]
+    birthDay = birthDate[0:2]
+
+    fDate = date(int(birthYear), int(birthMonth), int(birthDay))
+    lDate = date(int(todayYear), int(todayMonth), int(todayDay))
+
+    dayDifference = lDate - fDate
+
+    return (dayDifference.days / 365 > yearConstraint)

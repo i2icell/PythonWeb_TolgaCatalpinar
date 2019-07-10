8c9fef591046dd0b0692974d30972cbbfaf0e1dc
Exqra <tcatal99@hotmail.com>
2019-07-10 09:59:24 +0300

    10.07.2019 sabah

--- i2iCellApp/urls.py
+++ i2iCellApp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('homepage/', views.homepage, name='homepage'),
+    path('forgot-password/', views.forgotPassword, name='forgot_password'),
 
 ]
--- i2iCellApp/views.py
+++ i2iCellApp/views.py
@@ -1,22 +1,57 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.http import HttpResponse
+import requests
+from django.views.decorators.csrf import csrf_exempt
+import os
+from django.http import QueryDict
 # Create your views here.
 
-posts = [
-    {
-        'phoneNum': '555',
-        'password': '123'
-    },
-    {
-        'phoneNum': '223',
-        'password': '456'
-    }
-]
+proxies = {
+  "http": None,
+  "https": None,
+}
+
+endpoint = 'http://68.183.75.84:8080/i2iCellService/services/Services/login'
+
 def login(request):
-    context = {
-        'posts': posts
-    }
-    return render(request, 'i2iCellApp/login.html', context)
+    post = request.POST.copy()
+
+    if post.get('login_button'):
+        qd = QueryDict(mutable=True)
+        qd.update(
+            inputPhoneNumber=request.POST.get('phone_num'),
+            inputPassword=request.POST.get('password')
+        )
+
+        print("---------------You are in")
+        url = '{}?{}'.format(endpoint, qd.urlencode())
+        print("---------------Url: " + url)
+        response = requests.get(url, proxies=proxies)
+
+        #result = response.text
+        result = returnValidationValue(response.text)
+
+        if(result == '1'):
+            return render(request, 'i2iCellApp/homepage.html')
+        else:
+            messages.error(request, "Yanlis Kullanici Numarasi Veya Sifre!")
+
+    return render(request, 'i2iCellApp/login.html')
+
+def returnValidationValue( response):
+    searchedValue = "<ns:return>"
+
+    lengthOfSeachedValue = len(searchedValue)
+
+    startingIndexOfValue = response.index(searchedValue)
+    lengthOfReturnValue = 1
+
+    startingIndexOfReturnValue = startingIndexOfValue + lengthOfSeachedValue
+    return (response[startingIndexOfReturnValue: startingIndexOfReturnValue + lengthOfReturnValue])
 
 def homepage(request):
     return render(request, 'i2iCellApp/homepage.html')
+
+def forgotPassword(request):
+    return render(request, 'i2iCellApp/forgot_password.html')

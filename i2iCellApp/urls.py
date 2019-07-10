43801c8818f0fdc07638b1826d7a5c919849f4ce
Exqra <tcatal99@hotmail.com>
2019-07-10 16:09:32 +0300

    10.07.2019 sabah

--- i2iCellApp/urls.py
+++ i2iCellApp/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('homepage/', views.homepage, name='homepage'),
-    path('forgot-password/', views.forgotPassword, name='forgot_password'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('register/', views.register, name='register'),
 
 ]
--- i2iCellApp/views.py
+++ i2iCellApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.http import HttpResponse
+from requests.exceptions import ConnectionError
 import requests
 from django.views.decorators.csrf import csrf_exempt
 import os
@@ -35,8 +36,19 @@ def login(request):
         if(result == '1'):
             return render(request, 'i2iCellApp/homepage.html')
         else:
+            #Phone number not found
+            #Phone number and password does not match
+            #No connection
+            print(response)
+
             messages.error(request, "Yanlis Kullanici Numarasi Veya Sifre!")
 
+
+
+    elif post.get('forgot_button'):
+        return render(request, 'i2iCellApp/forgot_password.html')
+    elif post.get('register_button'):
+        return render(request, 'i2iCellApp/register.html')
     return render(request, 'i2iCellApp/login.html')
 
 def returnValidationValue( response):
@@ -55,3 +67,6 @@ def homepage(request):
 
 def forgotPassword(request):
     return render(request, 'i2iCellApp/forgot_password.html')
+
+def register(request):
+    return render(request, 'i2iCellApp/register.html')
